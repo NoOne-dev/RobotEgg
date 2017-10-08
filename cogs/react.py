c1af26ce0894a5d5aababad37e0600099ab09b0e
qtto <ottomuilwijk@live.nl>
2017-10-08 15:16:48 +0200

    parsing improvement

--- cogs/react.py
+++ cogs/react.py
@@ -41,12 +41,12 @@ class React:
             await self.bot.add_reaction(message, emoji)
 
         dontatme = ["dont at me", "don't at me", "dont @ me", "don't @ me"]
-        if message.content.lower() in dontatme:
+        if ' '.join(args[0:2]) in dontatme:
             channel = self.bot.get_channel('346601597335240704')  #bot-commands
             await self.bot.send_message(channel, f'<@!{message.author.id}>')
 
         dontatme = ["real eu hours", "real eu hour", "false eu hours", "real na hours", "real oce hours"]
-        if message.content.lower() in realeuhours:
+        if ' '.join(args[0:2]) in realeuhours:
             emoji = discord.utils.get(self.bot.get_all_emojis(), name='flag_eu')
             await self.bot.add_reaction(message, emoji)
 
