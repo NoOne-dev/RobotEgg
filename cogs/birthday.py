f5b53ca96b2f5363728071be18805b51b2b1dfec
qtto <ottomuilwijk@live.nl>
2017-10-24 17:15:42 +0200

    add age to bday notification

--- cogs/birthday.py
+++ cogs/birthday.py
@@ -45,7 +45,7 @@ class Birthday:
             return False
 
 
-    def _ordinal(n):
+    def _ordinal(self, n):
         return str(n)+("th" if 4 <= n%100 <= 20 else {1:"st", 2:"nd", 3:"rd"}.get(n%10, "th"))
 
 
