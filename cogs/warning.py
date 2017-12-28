90def43211f0c9bcae238089f3ea5b08f13df655
qtto <ottomuilwijk@live.nl>
2017-12-28 22:56:12 +0100

    Warnings -> strikes

--- cogs/warning.py
+++ cogs/warning.py
@@ -16,14 +16,14 @@ from sqlalchemy.orm import sessionmaker
 
 
 """
-Keeps track of warnings
+Keeps track of strikes
 """
 
 db = create_engine(os.environ['DATABASE_URL'])
 Base = declarative_base()
 
 
-class Warning_Table(Base):
+class Strike_Table(Base):
     __tablename__ = "warning_table"
     index = Column(Integer, primary_key=True, autoincrement=True)
     user_id = Column(String)
@@ -38,8 +38,8 @@ session = Session()
 Base.metadata.create_all(db)
 
 
-class Warning:
-    """Keep track of user warnings"""
+class Strike:
+    """Keep track of user strikes"""
     def __init__(self, bot):
         self.bot = bot
         self.queue = []
@@ -66,28 +66,28 @@ class Warning:
 
 
 
-    def _get_warning_message(self, user_id, ids=False):
-        """Generate message showing warnings"""
-        warnings = session.query(Warning_Table).filter_by(user_id=user_id).all()
+    def _get_strike_message(self, user_id, ids=False):
+        """Generate message showing strikes"""
+        strikes = session.query(Strike_Table).filter_by(user_id=user_id).all()
         id_list = []
         
-        message = f"**Warnings for <@!{user_id}>**\n"
+        message = f"**Strikes for <@!{user_id}>**\n"
         
-        if len(warnings) == 0:
-            message = "There are no warnings for this user."
+        if len(strikes) == 0:
+            message = "There are no strikes for this user."
         else:
             count = 1
-            for warning in warnings:
+            for strike in strikes:
                 if ids:
-                    id_list.append(warning.index)
-                    message += f"\n**Warning ID {warning.index}:**\n"
+                    id_list.append(strike.index)
+                    message += f"\n**Strike ID {strike.index}:**\n"
                 else:
-                    message += f"\n**Warning {count}:** \n"
-                message += f"    _Date:_ {warning.created_on.year}-{warning.created_on.month}-{warning.created_on.day}\n"
-                message += f"    _By:_ <@!{warning.created_by}>\n"
-                message += f"    _Reason:_ {warning.reason}\n"
-                if warning.notes:
-                    message += f"    _Notes:_ {warning.notes}\n\n"
+                    message += f"\n**Strike {count}:** \n"
+                message += f"    _Date:_ {strike.created_on.year}-{strike.created_on.month}-{strike.created_on.day}\n"
+                message += f"    _By:_ <@!{strike.created_by}>\n"
+                message += f"    _Reason:_ {strike.reason}\n"
+                if strike.notes:
+                    message += f"    _Notes:_ {strike.notes}\n\n"
                 count += 1
 
         if ids:
@@ -97,7 +97,7 @@ class Warning:
 
 
     async def _get_more_info(self, id_dict):
-        """Waits for user to provide a number to generate a warning list for"""
+        """Waits for user to provide a number to generate a strike list for"""
 
         def check(message):
             """Checks if the given user was present in the list"""
@@ -110,13 +110,13 @@ class Warning:
         if msg:
             user_id = id_dict[msg.content]
             await self._deletion_queue(msg)
-            msg = self._get_warning_message(user_id)
+            msg = self._get_strike_message(user_id)
             await self.bot.say(msg)
 
 
 
     async def _check_user(self, user, mod):
-        """Check if the correct user is being warned"""
+        """Check if the correct user is being strikeed"""
         def check(reaction, user):
             """Check if the reaction is by the bot and then if it's an OK or a not OK"""
             if user.id == msg.author.id:
@@ -124,7 +124,7 @@ class Warning:
             else:
                 return user.id == mod.id and (str(reaction.emoji) == '✅' or str(reaction.emoji) == '🛑')
 
-        msg = await self.bot.say(f"Warning: <@!{user.id}>. Is this correct?")
+        msg = await self.bot.say(f"Strike: <@!{user.id}>. Is this correct?")
 
         await self.bot.add_reaction(msg, '✅')
         await self.bot.add_reaction(msg, '🛑')
@@ -150,7 +150,7 @@ class Warning:
                 self.bot.say('Given reason is too long.')
             return len(message.content) > 5 and len(message.content) < 500
 
-        reason_msg = "Please provide a reason. Enter a message or choose a premade warning. Type 'stop' to cancel.\n"
+        reason_msg = "Please provide a reason. Enter a message or choose a premade one. Type 'stop' to cancel.\n"
         premade = {"1": "NSFW content",
                    "2": "Very disturbing content",
                    "3": "Use of slurs",
@@ -194,7 +194,7 @@ class Warning:
                 self.bot.say('Note is too long.')
             return len(message.content) < 500
 
-        msg = await self.bot.say(f"Optional: provide any notes or attachments (such as screenshots) or reply with 'done' to skip the wait.")
+        msg = await self.bot.say(f"Optional: provide any notes or attachments (such as screenshots) or reply with 'done' to skip.")
         await self._deletion_queue(msg)
 
         user_msg = await self.bot.wait_for_message(timeout=120.0, author=mod, check=check)
@@ -218,8 +218,8 @@ class Warning:
     @commands.command(pass_context=True)
     @channels_allowed(["mod-commands"])
     @is_mod()
-    async def warn(self, ctx):
-        """Add a warning to the database"""
+    async def strike(self, ctx):
+        """Add a strike to the database"""
         user = ctx.message.mentions
         if len(user) != 1:
             await self.bot.say(content=None, embed=create_error("- invalid user specified"))
@@ -244,55 +244,55 @@ class Warning:
 
         notes = '' if not notes else notes
 
-        warning = Warning_Table(
+        strike = Strike_Table(
             user_id=user.id,
             created_by=mod.id,
             created_on=date,
             reason=reason,
             notes=notes
         )
-        session.add(warning)
+        session.add(strike)
 
         try:
             session.commit() # Add it to the DB
         except Exception as e:
             print(e)
-            await self.bot.say(content=None, embed=create_error("entering warning into database: {e}"))
+            await self.bot.say(content=None, embed=create_error("entering strike into database: {e}"))
 
-        # Tell the mod and the user about the warning
-        count = session.query(Warning_Table).filter_by(user_id=user.id).count()
+        # Tell the mod and the user about the strike
+        count = session.query(Strike_Table).filter_by(user_id=user.id).count()
 
-        mod_message = f"<@!{mod.id}>, you have warned user <@!{user.id}>.\n\n"
+        mod_message = f"<@!{mod.id}> has given user <@!{user.id}> a strike.\n\n"
         mod_message += f"**Reason:** {reason}\n"
         if notes:
             mod_message += f"**Notes:** {notes}\n"
-        mod_message += f"\nUser has **{count} {'warnings' if count > 1 else 'warning'}**."
+        mod_message += f"\nUser has **{count} {'strikes' if count > 1 else 'strike'}**."
         await self.bot.say(mod_message)
         await self._deletion_queue(None, delete=True)
 
 
-        user_message = f"Hi {user.name},\n\nYou have received a warning in Eggserver Alpha.\n\n"
+        user_message = f"Hi {user.name},\n\nYou have received a strike in Eggserver Alpha.\n\n"
         user_message += f"**Reason:** {reason}.\n"
-        user_message += f"You have **{count} {'warnings' if count > 1 else 'warning'}**.\n\n"
+        user_message += f"You have **{count} {'strikes' if count > 1 else 'strike'}**.\n\n"
         user_message += f"If you have any further questions or concerns, please ask the mods."
         try:
             await self.bot.send_message(user, content=user_message)
         except Exception as e:
-            await self.bot.say(content=None, embed=create_error(f"DMing <@!{user.id}>. Please follow up, <@!{mod.id}> ({e})"))
+            await self.bot.say(content=None, embed=create_error(f"DMing <@!{user.id}>. Please follow up. <@!{mod.id}>, ({e})"))
 
 
 
     @commands.command(pass_context=True)
     @channels_allowed(["mod-commands"])
     @is_mod()
-    async def removewarning(self, ctx):
-        """Remove warning from user"""
+    async def removestrike(self, ctx):
+        """Remove strike from user"""
         def check(message):
-            """Check if the warning to remove is indeed valid"""
+            """Check if the strike to remove is indeed valid"""
             try:
                 return int(message.content) in ids
             except:
-                self.bot.say(content=None, embed=create_error("- enter a valid warning ID"))
+                self.bot.say(content=None, embed=create_error("- enter a valid strike ID"))
                 return False
 
         user = ctx.message.mentions
@@ -305,12 +305,12 @@ class Warning:
 
         user = user[0]
 
-        # Get the warnings and the warning IDs for the specified user
-        message, ids = self._get_warning_message(user.id, ids=True)
+        # Get the strikes and the strike IDs for the specified user
+        message, ids = self._get_strike_message(user.id, ids=True)
 
-        warn_msg = await self.bot.say(message)
-        await self._deletion_queue(warn_msg)
-        prompt_msg = await self.bot.say(content="Enter the ID of the warning to remove.")
+        strike_msg = await self.bot.say(message)
+        await self._deletion_queue(strike_msg)
+        prompt_msg = await self.bot.say(content="Enter the ID of the strike to remove.")
         await self._deletion_queue(prompt_msg)
 
         user_msg = await self.bot.wait_for_message(timeout=120.0, author=mod, check=check)
@@ -323,10 +323,10 @@ class Warning:
         removal_message = ''
         try:
             index = int(user_msg.content)
-            record = session.query(Warning_Table).get(index)
+            record = session.query(Strike_Table).get(index)
             session.delete(record)
             session.commit()
-            removal_message += f"<@!{mod.id}> removed warning from <@!{user.id}> with ID {index}.\n"
+            removal_message += f"<@!{mod.id}> removed strike from <@!{user.id}> with ID {index}.\n"
             removal_message += f"Removed item: {record.reason}"
             await self.bot.say(removal_message)
         except Exception as e:
@@ -340,36 +340,36 @@ class Warning:
     @commands.command(invoke_without_command=True)
     @channels_allowed(["mod-commands"])
     @is_mod()
-    async def warninglist(self):
-        """Generate complete list of warnings"""
+    async def table(self):
+        """Generate complete list of strikes"""
         message = '`,-------------------------------------------------------------.`\n'
         message += '`| #   | Amount  | User                                        |`\n'
         id_dict = {}
         count = 1
-        for row in session.query(Warning_Table.user_id).distinct():
+        for row in session.query(Strike_Table.user_id).distinct():
             row = row[0]
             id_dict[str(count)] = row
-            warnings = session.query(Warning_Table).filter_by(user_id=row).count()
-            warnings = f"`| {count}{((4-len(str(count)))*' ')}| {warnings}{((8-len(str(warnings)))*' ')}|`  <@!{row}>\n"
-            if len(message) + len(warnings) < 1000:
-                message += warnings
+            strikes = session.query(Strike_Table).filter_by(user_id=row).count()
+            strikes = f"`| {count}{((4-len(str(count)))*' ')}| {strikes}{((8-len(str(strikes)))*' ')}|`  <@!{row}>\n"
+            if len(message) + len(strikes) < 1000:
+                message += strikes
             else:
                 await self.bot.say(message)
-                message = warnings
+                message = strikes
             count += 1
 
         message += "`'-------------------------------------------------------------'`"
         await self.bot.say(message)
 
-        # If a mod enters a number give more info about the warnings of that user
+        # If a mod enters a number give more info about the strikes of that user
         await self._get_more_info(id_dict)
         await self._deletion_queue(None, delete=True)
 
 
 
     @commands.command(pass_context=True, invoke_without_command=True)
-    async def warnings(self, ctx):
-        """Check warnings of user or self"""
+    async def strikes(self, ctx):
+        """Check strikes of user or self"""
         user = ctx.message.mentions
         if len(user) > 1:
             await self.bot.say(content=None, embed=create_error("- too many users specified"))
@@ -381,7 +381,7 @@ class Warning:
         else:
             user = ctx.message.author
 
-        message = self._get_warning_message(user.id)
+        message = self._get_strike_message(user.id)
 
         if ctx.message.channel.id == config["channels"]["mod-commands"]:
             await self.bot.say(message)
@@ -389,10 +389,10 @@ class Warning:
             if ctx.message.author.id == user.id:
                 await self.bot.send_message(user, content=message)
             else:
-                await self.bot.say(content=None, embed=create_error("- you may only view your own warnings"))
+                await self.bot.say(content=None, embed=create_error("- you may only view your own strikes"))
 
 
 
 def setup(bot):
-    bot.add_cog(Warning(bot))
+    bot.add_cog(Strike(bot))
     
