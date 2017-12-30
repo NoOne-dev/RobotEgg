c896d6286cd2a257c509c1e00559ca23ca3a1aa4
qtto <ottomuilwijk@live.nl>
2017-12-30 22:55:48 +0100

    OCR start

---
+++ cogs/ocr.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+from config import config
+import pillow
+import pytesseract
+import cv2 
+
+
+class OCR:
+    """Finds @Mod in images"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    async def on_message(self, message):
+        if not message.attachments:
+            return
+
+        for attachment in message.attachments:
+            print(attachment)
+            
+
+# The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
+# When we load the cog, we use the name of the file.
+def setup(bot):
+    bot.add_cog(Ocr(bot))
+    
