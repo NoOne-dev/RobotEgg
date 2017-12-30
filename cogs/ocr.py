f61234a813b9bab548647f6b92c20d9805d19ac8
qtto <ottomuilwijk@live.nl>
2017-12-30 23:11:30 +0100

    load cog

--- cogs/ocr.py
+++ cogs/ocr.py
@@ -23,5 +23,5 @@ class OCR:
 # The setup fucntion below is neccesarry. Remember we give bot.add_cog() the name of the class in this case SimpleCog.
 # When we load the cog, we use the name of the file.
 def setup(bot):
-    bot.add_cog(Ocr(bot))
+    bot.add_cog(OCR(bot))
     
