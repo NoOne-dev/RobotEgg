a25bf8623ab906fff9fc83306470fb6897266d11
qtto <ottomuilwijk@live.nl>
2017-12-31 01:27:07 +0100

    load as json

--- cogs/ocr.py
+++ cogs/ocr.py
@@ -39,6 +39,8 @@ class OCR:
                 params = dict(apikey=self.API_KEY,
                               url=url)
                 text = await fetch(url, params=params)
+                text = json.loads(text)
+                
                 if text["FileParseExitCode"] == 1:
                     return text["ParsedText"]
                 else:
